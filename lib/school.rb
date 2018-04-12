250448b6570424aafb87edf6003563055d63ace5
paul.nicholsen <paul.nicholsen@flatironschool.com>
2018-04-12 16:28:09 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+	attr_reader :name, :roster
+
+	def initialize(name)
+		@name = name
+		@roster = {}
+	end
+
+	def add_student(name, grade)
+		if @roster[grade]
+			@roster[grade] << name
+		else
+			@roster[grade] = [name]
+		end
+	end
+
+	def grade(grade_level)
+		@roster[grade_level]
+	end
+
+	def sort
+		@roster.each do |grade, students|
+			students.sort!
+		end
+	end
+end
